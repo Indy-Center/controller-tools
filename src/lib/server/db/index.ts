bf18c593d5dbb7b0bbd6657345922eb991e8ba97
Steve Crow <steve@smcrow.com>
2024-12-29 18:34:14 -0500

    Check for building to prevent build from failing when dynamic env isn't present

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,7 +1,8 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import { env } from '$env/dynamic/private';
+import { building } from '$app/environment';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-const client = postgres(env.DATABASE_URL);
+if (!env.DATABASE_URL && !building) throw new Error('DATABASE_URL is not set');
+const client = postgres(env.DATABASE_URL!);
 export const db = drizzle(client);
