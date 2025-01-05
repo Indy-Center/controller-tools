243f9c502a943771ac6eeba4aaf319c8fdb909da
ztlhayden <haydenyounglex@hotmail.com>
2025-01-05 16:27:48 -0500

    implement filters sliding out of the bottom of the controls

--- src/routes/restrictions/FilterPanel.svelte
+++ src/routes/restrictions/FilterPanel.svelte
@@ -16,21 +16,21 @@
 	}
 </script>
 
-<div class="flex flex-col rounded-md lg:border lg:p-4 lg:dark:border-zinc-700">
+<div class="rounded-m relative flex flex-col">
 	<!-- Filter Header -->
-	<div class="flex gap-1 py-2 lg:px-2">
+	<div class="flex gap-1 py-2">
 		<input
 			type="text"
 			id="filter"
-			class="w-full rounded-md border border-zinc-300 p-3 text-zinc-700 dark:bg-zinc-700 dark:text-zinc-200"
+			class="w-full rounded-md border border-zinc-300 p-3 text-zinc-700"
 			placeholder="Search for Airport..."
 			bind:value={restrictionFilters.airport}
 		/>
 	</div>
 
 	<!-- Quick  Controls -->
-	<div class="around flex justify-between px-1 py-2 lg:px-2">
-		<div class="flex flex-col gap-2">
+	<div class="flex justify-center px-1 py-2">
+		<div class="mr-auto flex flex-col gap-2">
 			<label for="includeIncoming" class="pl-2 text-sm">
 				<input
 					type="checkbox"
@@ -46,25 +46,20 @@
 		</div>
 		<button
 			id="filterSlider"
-			class="rounded-md border p-1 text-3xl"
+			class="mr-2 rounded-md border p-1 text-3xl hover:bg-zinc-200 active:bg-zinc-300"
 			onclick={() => (drawerOpen = !drawerOpen)}><MdiFilterCogOutline /></button
 		>
 	</div>
-	<div class:hidden={!drawerOpen} class="flex max-w-full flex-col lg:p-2">
-		<p class="p-4 text-center">
-			Read more about this tool on the <a
-				href="https://wiki.zidartcc.org/docs/"
-				target="_blank"
-				class="underline"
-				rel="noopener noreferrer">Indy Library</a
-			>.
-		</p>
+	<div
+		class:scale-y-100={drawerOpen}
+		class="bg absolute top-full z-40 flex w-full origin-top scale-y-0 flex-col overflow-hidden transition-all"
+	>
 		{#each areaMap as [category, areas]}
 			<h2 class="text-lg font-medium">{category}</h2>
 			<div class="flex flex-wrap gap-2">
 				{#each areas as area}
 					<button
-						class="rounded-md border p-1 text-sm lg:px-2 lg:text-base dark:border-zinc-700 dark:text-zinc-200"
+						class="rounded-md border p-1 text-sm"
 						class:bg-zinc-200={restrictionFilters.areas.includes(area.id)}
 						class:dark:bg-zinc-700={restrictionFilters.areas.includes(area.id)}
 						onclick={() => toggleAreaFilter(area.id)}>{area.label}</button
