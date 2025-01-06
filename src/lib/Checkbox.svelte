bdd1381b07fb955b4786824b6d84c04400a6caea
ztlhayden <haydenyounglex@hotmail.com>
2025-01-06 00:24:08 -0500

    implement checkbox component

---
+++ src/lib/Checkbox.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	let { id, label, checked = $bindable(), disabled = false } = $props();
+</script>
+
+<label for={id} class="text-md flex items-center space-x-1">
+	<input
+		class="form-checkbox h-4 w-4 rounded border-gray-400 bg-gray-200 align-middle text-gray-600 accent-zinc-600 focus:ring-gray-500"
+		type="checkbox"
+		{id}
+		{disabled}
+	/>
+	<span class="mt-[-2px] align-middle">{label}</span>
+</label>
