9c554f508799e62728074a6c7e1b7765178cd32f
ztlhayden <haydenyounglex@hotmail.com>
2025-01-07 00:34:24 -0500

    initial notes formating

--- src/routes/restrictions/Notes.svelte
+++ src/routes/restrictions/Notes.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import Turbine from 'virtual:icons/mdi/turbine';
 	import Fan from 'virtual:icons/mdi/fan';
-	import StarFourPointsSmall from 'virtual:icons/mdi/star-four-points-small';
+	import StarFourPoints from 'virtual:icons/mdi/star-four-points';
 	import MapMarkerPath from 'virtual:icons/mdi/map-marker-path';
 	import Handshake from 'virtual:icons/mdi/handshake';
 	import HandPointingRight from 'virtual:icons/mdi/hand-pointing-right';
+	import ArrowLeftThin from 'virtual:icons/mdi/arrow-left-thin';
+	import MapMarkerRight from 'virtual:icons/mdi/map-marker-right';
 	import Radar from 'virtual:icons/mdi/radar';
 	import Note from 'virtual:icons/mdi/note';
 
@@ -14,10 +16,11 @@
 	const badgeConfig = [
 		{ match: 'JET', icon: Turbine, label: 'JET' },
 		{ match: 'PROP', icon: Fan, label: 'PROP' },
-		{ match: 'RNAV1', icon: StarFourPointsSmall, label: 'RNAV 1' },
+		{ match: 'RNAV1', icon: StarFourPoints, label: 'RNAV 1' },
 		{ match: 'NON-PBN', icon: MapMarkerPath, label: 'Non-PBN' },
 		{ match: 'AIT', icon: Handshake, label: 'AIT' },
 		{ match: 'APO', icon: HandPointingRight, label: 'APO' },
+		{ match: 'LAST', icon: MapMarkerRight, label: 'JOIN BY' },
 		{ match: 'CONTROL', icon: Radar, label: 'CONTROL' }
 	];
 
@@ -53,20 +56,22 @@
 
 <div class="space-y-2">
 	{#each conditions.filter((c) => c.label !== 'OTHER') as { icon: Icon, label, content }}
-		<div class="flex w-full flex-col items-start gap-1 rounded-lg border border-gray-300 p-2">
+		<div
+			class="flex w-full flex-wrap items-start gap-1 rounded-lg border border-gray-300 bg-indigo-600 bg-opacity-80 p-2 text-white"
+		>
 			<div class="flex items-center gap-1">
-				<Icon class="h-5 w-5 text-gray-700 dark:text-gray-200" />
-				<span class="text-sm font-semibold text-gray-700 dark:text-gray-100">{label}</span>
+				<Icon class="h-5 w-5" />
+				<span class="-mt-[2px] text-sm font-semibold">{label + (content ? ':' : '')}</span>
 			</div>
 			{#if content}
-				<div class="text-left text-sm text-gray-600 dark:text-gray-400">{content}</div>
+				<div class="-mt-[1px] text-left text-sm">{content}</div>
 			{/if}
 		</div>
 	{/each}
 
 	{#each conditions as { label, content }}
 		{#if label === 'OTHER' && content}
-			<div class="text-left text-sm text-gray-700 dark:text-gray-300">{content}</div>
+			<div class="text-left text-sm">{content}</div>
 		{/if}
 	{/each}
 </div>
