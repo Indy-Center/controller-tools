bddf9187fd1cbdfdcdf4c13e35b8f40c0bb9fa11
ztlhayden <haydenyounglex@hotmail.com>
2025-01-07 18:28:58 -0500

    lg breakpoint

--- src/routes/restrictions/RestrictionRow.svelte
+++ src/routes/restrictions/RestrictionRow.svelte
@@ -7,28 +7,26 @@
 	let { route, restrictions }: { route: string; restrictions: Restriction[] } = $props();
 </script>
 
-<div
-	class="flex flex-col rounded-sm px-2 py-2 pb-2 odd:bg-zinc-200 md:flex-row dark:odd:bg-zinc-700"
->
+<div class="flex flex-col rounded-sm p-2 odd:bg-zinc-200 md:flex-row lg:gap-0 dark:odd:bg-zinc-700">
 	<!-- Route Header -->
-	<div class=" flex flex-col pb-1 md:w-4/6 lg:w-4/12">
+	<div class="flex flex-col pb-1 md:w-4/6 lg:w-4/12">
 		<span class="block font-light lg:hidden">Route</span>
 		{route || 'All Routes'}
 	</div>
 
 	<!-- Restrictions Data -->
-	<div class="flex w-auto flex-grow flex-col md:w-5/6 lg:w-full">
+	<div class="flex flex-col gap-4 md:w-5/6 lg:w-8/12">
 		{#each restrictions as restriction}
 			<div
-				class="flex flex-grow flex-col border-zinc-200 pb-4 last:pb-0 md:gap-1 lg:flex-row"
+				class="flex flex-col border-zinc-200 pb-4 last:pb-0 md:gap-1 lg:flex-row lg:gap-0"
 				class:incoming={restrictionFilters.includeIncoming &&
 					restrictionFilters.areas.includes(restriction.to.id) &&
 					restrictionFilters.dimIncoming}
 			>
 				<!-- sectors -->
-				<div class="flex w-full gap-2">
+				<div class="flex gap-2 lg:w-1/4">
 					<!-- From Header -->
-					<div class="flex w-full flex-col justify-center">
+					<div class="flex w-full flex-col justify-center lg:justify-start">
 						{#if restriction.from}
 							<span class="block font-light lg:hidden">From</span>
 							<AreaBadge label={restriction.from.id} />
@@ -36,7 +34,7 @@
 					</div>
 
 					<!-- To Header -->
-					<div class="flex w-full flex-col justify-center">
+					<div class="flex w-full flex-col justify-center lg:justify-start">
 						{#if restriction.to}
 							<span class="block font-light lg:hidden">To</span>
 							<AreaBadge label={restriction.to.id} />
@@ -45,13 +43,13 @@
 				</div>
 
 				<!-- Restriction Header -->
-				<div class="flex w-full flex-col justify-center">
+				<div class="flex w-full flex-col justify-center lg:w-1/3 lg:justify-start lg:px-4">
 					<span class="block font-light lg:hidden">Restriction</span>
 					{restriction.restriction || 'Route only'}
 				</div>
 
 				<!-- Notes Header -->
-				<div class="flex w-full flex-col justify-center">
+				<div class="flex w-full flex-col justify-center lg:w-1/3 lg:justify-start">
 					{#if restriction.notes}
 						<span class="block font-light lg:hidden">Notes</span>
 						<Notes content={restriction.notes || ''} />
--- src/routes/restrictions/RestrictionSection.svelte
+++ src/routes/restrictions/RestrictionSection.svelte
@@ -11,7 +11,7 @@
 			if (!map.has(route)) {
 				map.set(route, []);
 			}
-			map.get(route).push(restriction);
+			map.get(route)!.push(restriction);
 		});
 		return map;
 	});
@@ -21,18 +21,22 @@
 	class="mb-3 flex w-full flex-col rounded-md border-2 border-t-0 border-zinc-600 md:mb-6 md:flex-row md:border-l-0 md:border-t-2 lg:border-none"
 >
 	<div
-		class="w-full rounded-sm bg-zinc-600 text-center text-lg font-normal text-white md:w-24 md:rounded-md"
+		class="w-full rounded-sm bg-zinc-600 text-center text-lg font-normal text-white md:w-24 md:rounded-md lg:mt-2"
 	>
 		{airport}
 	</div>
 	<div class="flex flex-grow flex-col gap-4">
-		<div class="hidden border-b border-b-zinc-500 px-2 font-medium lg:flex dark:border-b-zinc-300">
+		<div
+			class="mx-2 -mb-4 hidden border-b border-b-zinc-500 font-medium lg:flex dark:border-b-zinc-300"
+		>
 			<div class="w-4/12">Route</div>
-			<div class="flex flex-grow">
-				<div class="w-1/12">From</div>
-				<div class="w-1/12">To</div>
-				<div class="w-3/12">Restriction</div>
-				<div class="w-3/12">Notes</div>
+			<div class="flex w-8/12 flex-grow">
+				<div class="flex w-1/4 gap-2">
+					<div class="w-full">From</div>
+					<div class="w-full">To</div>
+				</div>
+				<div class="w-1/3 px-4">Restriction</div>
+				<div class="w-1/3">Notes</div>
 			</div>
 		</div>
 		{#each routes as [route, restrictions]}
