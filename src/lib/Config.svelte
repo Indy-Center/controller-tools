e7f64fac051680844464ab79c7bde7c81b151cc6
ztlhayden <haydenyounglex@hotmail.com>
2025-01-12 19:58:10 -0500

    working push

--- src/lib/Config.svelte
+++ src/lib/Config.svelte
@@ -2,7 +2,14 @@
 	import ModalPopup from './ModalPopup.svelte';
 	import MdiCog from 'virtual:icons/mdi/cog';
 	import ThemeToggle from './ThemeToggle.svelte';
+	import IndyLogo from '$lib/IndyLogo.svelte';
+	import MdiBookOpenVariant from 'virtual:icons/mdi/book-open-variant';
+	import MdiAccountConvert from 'virtual:icons/mdi/account-convert';
+	import logo from '$lib/assets/vnas-logo.png';
+	import ControllerChangeDiag from './ControllerChangeDiag.svelte';
+	import PopupModal from './ModalPopup.svelte';
 	let modal: any;
+	let controllerPopup: any;
 </script>
 
 <div class="align-center flex justify-end">
@@ -13,26 +20,72 @@
 
 <ModalPopup closeButton={true} bind:this={modal}>
 	<div class="flex flex-col items-center gap-4 text-xl">
-		<h3 class="mb-4 text-2xl font-bold">Settings</h3>
-		<div class="flex items-center gap-2">
-			<span>Toggle Theme:</span>
-			<ThemeToggle />
+		<div class="hidden md:flex xl:basis-0">
+			<div id="buttons" class="align-center ml-6 flex gap-3">
+				<div class="tooltip w-8">
+					<a class="text-3xl text-white" href="https://zidartcc.org" target="_blank">
+						<IndyLogo />
+						<span class="tooltip-text primary text-sm">Indy Center Website</span>
+					</a>
+				</div>
+				<div class="tooltip">
+					<a
+						href="https://wiki.zidartcc.org/docs/home"
+						target="_blank"
+						class="text-3xl text-white"
+						aria-label="Controller Procedures"
+					>
+						<MdiBookOpenVariant />
+						<span class="tooltip-text primary text-sm">vZID Controller Documents</span>
+					</a>
+				</div>
+				<div class="tooltip w-8">
+					<a
+						href="https://crc.virtualnas.net/docs/#/"
+						target="_blank"
+						aria-label="CRC Documentation"
+						class=""
+					>
+						<img src={logo} alt="vNAS Logo" width="30px" class="pt-1" />
+						<span class="tooltip-text bg-zinc-900 text-sm">CRC Documentation</span>
+					</a>
+				</div>
+				<div class="tooltip">
+					<button
+						onclick={() => controllerPopup.openModal()}
+						aria-label="controller change popup"
+						class="block text-3xl text-white"
+					>
+						<MdiAccountConvert />
+						<span class="tooltip-text bg-zinc-900 text-sm">Controller Change Procedures</span>
+					</button>
+				</div>
+				<h3 class="mb-4 text-2xl font-bold">Settings</h3>
+				<div class="flex items-center gap-2">
+					<span>Toggle Theme:</span>
+					<ThemeToggle />
+				</div>
+				<a
+					href="http://wiki.zidartcc.org/docs/icct"
+					target="_blank"
+					rel="noopener noreferrer"
+					class="underline hover:no-underline"
+				>
+					Read the user manual
+				</a>
+				<a
+					href="https://github.com/Indy-Center/controller-tools/issues"
+					target="_blank"
+					rel="noopener noreferrer"
+					class="underline hover:no-underline"
+				>
+					Report Issues on GitHub
+				</a>
+			</div>
 		</div>
-		<a
-			href="http://wiki.zidartcc.org/docs/icct"
-			target="_blank"
-			rel="noopener noreferrer"
-			class="underline hover:no-underline"
-		>
-			Read the user manual
-		</a>
-		<a
-			href="https://github.com/Indy-Center/controller-tools/issues"
-			target="_blank"
-			rel="noopener noreferrer"
-			class="underline hover:no-underline"
-		>
-			Report Issues on GitHub
-		</a>
-	</div>
-</ModalPopup>
+	</div></ModalPopup
+>
+
+<PopupModal closeButton={true} bind:this={controllerPopup}>
+	<ControllerChangeDiag />
+</PopupModal>
--- src/lib/NavTabs.svelte
+++ src/lib/NavTabs.svelte
@@ -1,12 +1,6 @@
 <script lang="ts">
-	import IndyLogo from '$lib/IndyLogo.svelte';
 	import { page } from '$app/state';
-	import logo from '$lib/assets/vnas-logo.png';
 	import MdiDotsVertical from 'virtual:icons/mdi/dots-vertical';
-	import MdiBookOpenVariant from 'virtual:icons/mdi/book-open-variant';
-	import MdiAccountConvert from 'virtual:icons/mdi/account-convert';
-	import ControllerChangeDiag from './ControllerChangeDiag.svelte';
-	import PopupModal from './ModalPopup.svelte';
 	import Config from './Config.svelte';
 	import type { User } from '$lib/server/session';
 
@@ -14,22 +8,18 @@
 
 	// state for mobile menu
 	let menuActive = $state(false);
-	let controllerPopup: any;
 </script>
 
 <nav
 	class="primary relative flex h-14 w-full flex-row items-center justify-between px-4 shadow-md md:h-16 xl:justify-center"
 >
-	<PopupModal closeButton={true} bind:this={controllerPopup}>
-		<ControllerChangeDiag />
-	</PopupModal>
 	<button
 		class="ml-[-14px] text-3xl text-white md:hidden"
 		aria-label="mobile nav menu"
-		onclick={() => (menuActive = !menuActive)}>
-		<MdiDotsVertical />
-	</button
+		onclick={() => (menuActive = !menuActive)}
 	>
+		<MdiDotsVertical />
+	</button>
 	<a href="/" class="text-2xl font-medium text-white hover:text-zinc-200 lg:pr-4">
 		<h1>ICCT</h1>
 	</a>
@@ -57,48 +47,7 @@
 			</a>
 		</div>
 	{/if}
-	<div class="hidden md:flex xl:basis-0">
-		<div id="buttons" class="align-center ml-6 flex gap-3">
-			<div class="tooltip w-8">
-				<a class="text-3xl text-white" href="https://zidartcc.org" target="_blank">
-					<IndyLogo />
-					<span class="tooltip-text primary text-sm">Indy Center Website</span>
-				</a>
-			</div>
-			<div class="tooltip">
-				<a
-					href="https://wiki.zidartcc.org/docs/home"
-					target="_blank"
-					class="text-3xl text-white"
-					aria-label="Controller Procedures"
-				>
-					<MdiBookOpenVariant />
-					<span class="tooltip-text primary text-sm">vZID Controller Documents</span>
-				</a>
-			</div>
-			<div class="tooltip w-8">
-				<a
-					href="https://crc.virtualnas.net/docs/#/"
-					target="_blank"
-					aria-label="CRC Documentation"
-					class=""
-				>
-					<img src={logo} alt="vNAS Logo" width="30px" class="pt-1" />
-					<span class="tooltip-text bg-zinc-900 text-sm">CRC Documentation</span>
-				</a>
-			</div>
-			<div class="tooltip">
-				<button
-					onclick={() => controllerPopup.openModal()}
-					aria-label="controller change popup"
-					class="block text-3xl text-white"
-				>
-					<MdiAccountConvert />
-					<span class="tooltip-text bg-zinc-900 text-sm">Controller Change Procedures</span>
-				</button>
-			</div>
-		</div>
-	</div>
+
 	<ul
 		id="tabs"
 		class:scale-y-100={menuActive}
@@ -113,60 +62,66 @@
 				>
 					{link.displayName}
 				</a>
-				<div class="tab-line"
-						 class:current-tab={link.href === '/' ? page.url.pathname === '/' : page.url.pathname.startsWith(link.href)}>
-				</div>
+				<div
+					class="tab-line"
+					class:current-tab={link.href === '/'
+						? page.url.pathname === '/'
+						: page.url.pathname.startsWith(link.href)}
+				></div>
 			</li>
 		{/each}
 	</ul>
+
+	<Config />
 </nav>
+
 <style>
-    #tabs .tab-line.current-tab {
-        width: 90%;
-        @media (max-width: 765px) {
-            width: 45%;
-        }
-    }
+	#tabs .tab-line.current-tab {
+		width: 90%;
+		@media (max-width: 765px) {
+			width: 45%;
+		}
+	}
 
-    #tabs li:hover .tab-line:not(.current-tab) {
-        width: 30%;
-    }
+	#tabs li:hover .tab-line:not(.current-tab) {
+		width: 30%;
+	}
 
-    #tabs li:active .tab-line:not(.current-tab) {
-        width: 80%;
-    }
+	#tabs li:active .tab-line:not(.current-tab) {
+		width: 80%;
+	}
 
-    #tabs .tab-line {
-        width: 0%;
-        height: 3px;
-        background-color: currentColor;
-        border-radius: 2px;
-        transition: width 0.3s ease;
-        margin: 0 auto;
-    }
+	#tabs .tab-line {
+		width: 0%;
+		height: 3px;
+		background-color: currentColor;
+		border-radius: 2px;
+		transition: width 0.3s ease;
+		margin: 0 auto;
+	}
 
-    #buttons .tooltip {
-        position: relative;
-        display: inline-block;
-    }
+	#buttons .tooltip {
+		position: relative;
+		display: inline-block;
+	}
 
-    #buttons .tooltip .tooltip-text {
-        visibility: hidden;
-        width: 120px;
-        background-color: black;
-        color: #fff;
-        text-align: center;
-        border-radius: 0.5rem;
-        padding: 3px 0;
-        top: 107%;
-        left: 50%;
-        margin-left: -60px;
-        position: absolute;
-        z-index: 40;
-        opacity: 91%;
-    }
+	#buttons .tooltip .tooltip-text {
+		visibility: hidden;
+		width: 120px;
+		background-color: black;
+		color: #fff;
+		text-align: center;
+		border-radius: 0.5rem;
+		padding: 3px 0;
+		top: 107%;
+		left: 50%;
+		margin-left: -60px;
+		position: absolute;
+		z-index: 40;
+		opacity: 91%;
+	}
 
-    #buttons .tooltip:hover .tooltip-text {
-        visibility: visible;
-    }
+	#buttons .tooltip:hover .tooltip-text {
+		visibility: visible;
+	}
 </style>
