48bc56f9e60736227750a1d017b17528c3da0831
Steve Crow <steve@smcrow.com>
2025-06-24 20:37:59 -0400

    Deduplicate charts from the old charts api

--- src/routes/api/charts/[id]/+server.ts
+++ src/routes/api/charts/[id]/+server.ts
@@ -6,6 +6,10 @@ export async function GET({ params }) {
 	).then((res) => res.json());
 
 	// Return the chart data. The key doesn't matter since we're only fetching one airport.
-	// We'll merge all the results together into a single array.
-	return json(Object.values(charts).flat());
+	// We'll merge all the results together into a single array and deduplicate.
+	const allCharts = Object.values(charts).flat();
+	const uniqueCharts = allCharts.filter((chart, index, arr) => 
+		arr.findIndex(c => c.chart_code === chart.chart_code && c.chart_name === chart.chart_name) === index
+	);
+	return json(uniqueCharts);
 }
